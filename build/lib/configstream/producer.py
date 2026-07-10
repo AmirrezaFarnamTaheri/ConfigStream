6572a70a148e5f983c97e92d4f77c69ea0b08418
Farnam <taheri.farnam@gmail.com>
2026-07-10 23:11:44 +0330

    chore: remove remaining generated build artifacts

--- build/lib/configstream/producer.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
-"""
-Backward-compatible forwarder for legacy producer attributes.
-"""
-
-from configstream.pipeline.fetcher import fetch_multiple_sources
-from configstream.async_file_ops import read_multiple_files_async
-
-__all__ = ["fetch_multiple_sources", "read_multiple_files_async"]
